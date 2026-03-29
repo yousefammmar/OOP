d37bc61980d26735ca607123bc86263324ca3240
yousofamjed <yousofamjed@gmail.com>
2026-03-29 10:50:13 +0300

    Update SettersandGetters2.cpp

--- code/SettersandGetters2.cpp
+++ code/SettersandGetters2.cpp
@@ -1,179 +1,179 @@
-//#include<iostream>
-//#include<string>
-//using namespace std;
-//
-//
-//class Rectangle
-//{
-//public:
-//
-//	//constructor
-//	Rectangle()
-//	{
-//		height_ = 0;
-//		width_ = 0;
-//	}
-//
-///* example for setters1
-//	void setWidth(int width)
-//	{
-//		if (width <= 0)//check the parameter value
-//		{
-//			cout << "Error, the width must not be less than or equal to zero" << endl;
-//			exit(0);
-//		}
-//		else
-//			width_ = width;// data member=parameter
-//	}
-//		
-//	void setHeight(int height)
-//	{
-//		if (height <= 0)//check the parameter value
-//		{
-//			cout << "Error, the height must not be less than or equal to zero" << endl;
-//			exit(0);
-//		}
-//		else
-//			height_ = height;// data member=parameter 
-//	}
-//*/
-//
-//
-///* example for setters2
-//bool setWidth(int width)
-//	{
-//		if (width <= 0)//check the parameter value
-//		{
-//			return false;
-//		}
-//		else
-//		{
-//			width_ = width;// data member=parameter
-//			return true;
-//		}
-//	}
-//
-//	bool setHeight(int height)
-//	{
-//		if (height <= 0)//check the parameter value
-//		{
-//			return false;
-//		}
-//		else
-//		{
-//			height_ = height;// data member=parameter 
-//			return true;
-//		}
-//	}
-//	*/
-//
-//
-///* example for setters3
-//	void setWidth()
-//	{
-//		int width = 0;
-//
-//		while (width <= 0)
-//		{
-//			cout << "Enter the width of the rectangle: ";
-//			cin >> width;
-//		}
-//
-//		width_ = width;
-//	}
-//
-//	void setHeight()
-//	{
-//		int height = 0;
-//
-//		while (height <= 0)
-//		{
-//			cout << "Enter the height of the rectangle: ";
-//			cin >> height;
-//		}
-//
-//		height_ = height;
-//	}
-//	*/
-//
-//	int getWidth()
-//	{
-//		return width_;
-//	}
-//
-//	int getHeight()
-//	{
-//		return height_;
-//	}
-//
-//
-//
-//	void printArea()
-//	{
-//		cout << "The area of rectangle 1 with height: " << height_ << " and width: " << width_ << " is:" << calculateArea() << endl << endl;
-//	}
-//
-//private:
-//	int height_;
-//	int width_;
-//
-//    int calculateArea()
-//	{
-//		return height_ * width_;
-//	}
-//};
-//
-//int main()
-//{
-//	Rectangle r1;
-//
-//	
-//	/*Setters with example1
-//	int h;
-//	int w;
-//	cout << "Enter the height of the rectangle:";
-//	cin >> h;
-//	r1.setHeight(h);
-//
-//	cout << "Enter the width of the rectangle:";
-//	cin >> w;
-//	r1.setWidth(w);
-//	*/
-//
-//	/*setters with example 2
-//	int h;
-//	int w;
-//	bool flag;
-//
-//	cout << "Enter the height of the rectangle:";
-//	cin >> h;
-//
-//
-//	flag = r1.setHeight(h);
-//	if (flag == false)
-//	{
-//		cout << "Height must be more than 0" << endl;
-//	}
-//
-//	cout << "Enter the width of the rectangle:";
-//	cin >> w;
-//	flag = r1.setWidth(w);
-//	if (flag == false)
-//	{
-//		cout << "Width must be more than 0" << endl;
-//	}
-//	*/
-//
-//	/* setters with example 3
-//	r1.setHeight();
-//	r1.setWidth();
-//	*/
-//
-//
-//	cout << "The width of the rectangle is: " << r1.getWidth() << endl;
-//	cout << "The height of the rectangle is: " << r1.getHeight() << endl;
-//
-//
-//
-//	r1.printArea();
-//}
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+class Rectangle
+{
+public:
+
+	//constructor
+	Rectangle()
+	{
+		height_ = 0;
+		width_ = 0;
+	}
+
+/* example for setters1
+	void setWidth(int width)
+	{
+		if (width <= 0)//check the parameter value
+		{
+			cout << "Error, the width must not be less than or equal to zero" << endl;
+			exit(0);
+		}
+		else
+			width_ = width;// data member=parameter
+	}
+		
+	void setHeight(int height)
+	{
+		if (height <= 0)//check the parameter value
+		{
+			cout << "Error, the height must not be less than or equal to zero" << endl;
+			exit(0);
+		}
+		else
+			height_ = height;// data member=parameter 
+	}
+*/
+
+
+/* example for setters2
+bool setWidth(int width)
+	{
+		if (width <= 0)//check the parameter value
+		{
+			return false;
+		}
+		else
+		{
+			width_ = width;// data member=parameter
+			return true;
+		}
+	}
+
+	bool setHeight(int height)
+	{
+		if (height <= 0)//check the parameter value
+		{
+			return false;
+		}
+		else
+		{
+			height_ = height;// data member=parameter 
+			return true;
+		}
+	}
+	*/
+
+
+/* example for setters3
+	void setWidth()
+	{
+		int width = 0;
+
+		while (width <= 0)
+		{
+			cout << "Enter the width of the rectangle: ";
+			cin >> width;
+		}
+
+		width_ = width;
+	}
+
+	void setHeight()
+	{
+		int height = 0;
+
+		while (height <= 0)
+		{
+			cout << "Enter the height of the rectangle: ";
+			cin >> height;
+		}
+
+		height_ = height;
+	}
+	*/
+
+	int getWidth()
+	{
+		return width_;
+	}
+
+	int getHeight()
+	{
+		return height_;
+	}
+
+
+
+	void printArea()
+	{
+		cout << "The area of rectangle 1 with height: " << height_ << " and width: " << width_ << " is:" << calculateArea() << endl << endl;
+	}
+
+private:
+	int height_;
+	int width_;
+
+   int calculateArea()
+	{
+		return height_ * width_;
+	}
+};
+
+int main()
+{
+	Rectangle r1;
+
+	
+	/*Setters with example1
+	int h;
+	int w;
+	cout << "Enter the height of the rectangle:";
+	cin >> h;
+	r1.setHeight(h);
+
+	cout << "Enter the width of the rectangle:";
+	cin >> w;
+	r1.setWidth(w);
+	*/
+
+	/*setters with example 2
+	int h;
+	int w;
+	bool flag;
+
+	cout << "Enter the height of the rectangle:";
+	cin >> h;
+
+
+	flag = r1.setHeight(h);
+	if (flag == false)
+	{
+		cout << "Height must be more than 0" << endl;
+	}
+
+	cout << "Enter the width of the rectangle:";
+	cin >> w;
+	flag = r1.setWidth(w);
+	if (flag == false)
+	{
+		cout << "Width must be more than 0" << endl;
+	}
+	*/
+
+	/* setters with example 3
+	r1.setHeight();
+	r1.setWidth();
+	*/
+
+
+	cout << "The width of the rectangle is: " << r1.getWidth() << endl;
+	cout << "The height of the rectangle is: " << r1.getHeight() << endl;
+
+
+
+	r1.printArea();
+}
