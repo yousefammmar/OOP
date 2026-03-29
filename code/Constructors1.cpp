d567c716ee3d8e81bf974480033499adb7216ef9
yousofamjed <yousofamjed@gmail.com>
2026-03-29 10:43:21 +0300

    Update Constructors1.cpp

--- code/Constructors1.cpp
+++ code/Constructors1.cpp
@@ -1,91 +1,91 @@
-//#include <iostream>
-//#include <string>
-//using namespace std;
-//
-//class Student
-//{
-//private:
-//	int id_;
-//	string name_;
-//
-//public:
-//
-//	//default constructor
-//	Student()
-//	{
-//		id_ = 0;
-//		name_ = "---";
-//	}
-//
-//	//parameterized constructor
-//	Student(int id, string name)
-//	{		
-//		id_ = id;
-//		name_ = name;
-//		
-//		//or
-//		/*
-//		setId(id);
-//		setName(name);		
-//		*/
-//	}
-//
-//	void setId(int id)
-//	{
-//		if (id < 0)
-//		{
-//			cout << "The id must not be negative" << endl;
-//			exit(0);
-//		}
-//		else
-//			id_ = id;
-//	}
-//	void setName(string name)
-//	{
-//		if (name.length() == 0)
-//		{
-//			cout << "The lenght of name must be more than zero letters" << endl;
-//			exit(0);
-//		}
-//		else
-//			name_ = name;
-//	}
-//
-//	int getId()
-//	{
-//		return id_;
-//	}
-//
-//	string getName()
-//	{
-//		return name_;
-//	}
-//
-//	void printStudent()
-//	{
-//		cout << "Student Id is: " << id_ << endl;
-//
-//		cout << "Student name is: " << name_ << endl << endl;
-//	}
-//};
-//
-//int main()
-//{
-//	Student s1; //this will call the default constructor
-//	s1.printStudent();
-//
-//	s1.setId(1);
-//	s1.setName("Ali");
-//
-//	s1.printStudent();
-//	
-//
-//	Student s2(2,"Ahmed"); //this will call the parameterized constructor
-//	s2.printStudent();
-//	s2.setName("Mahmoud");
-//
-//	s2.printStudent();
-//
-//
-//
-//}
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student
+{
+private:
+	int id_;
+	string name_;
+
+public:
+
+	//default constructor
+	Student()
+	{
+		id_ = 0;
+		name_ = "---";
+	}
+
+	//parameterized constructor
+	Student(int id, string name)
+	{		
+		id_ = id;
+		name_ = name;
+		
+		//or
+		/*
+		setId(id);
+		setName(name);		
+		*/
+	}
+
+	void setId(int id)
+	{
+		if (id < 0)
+		{
+			cout << "The id must not be negative" << endl;
+			exit(0);
+		}
+		else
+			id_ = id;
+	}
+	void setName(string name)
+	{
+		if (name.length() == 0)
+		{
+			cout << "The lenght of name must be more than zero letters" << endl;
+			exit(0);
+		}
+		else
+			name_ = name;
+	}
+
+	int getId()
+	{
+		return id_;
+	}
+
+	string getName()
+	{
+		return name_;
+	}
+
+	void printStudent()
+	{
+		cout << "Student Id is: " << id_ << endl;
+
+		cout << "Student name is: " << name_ << endl << endl;
+	}
+};
+
+int main()
+{
+	Student s1; //this will call the default constructor
+	s1.printStudent();
+
+	s1.setId(1);
+	s1.setName("Ali");
+
+	s1.printStudent();
+	
+
+	Student s2(2,"Ahmed"); //this will call the parameterized constructor
+	s2.printStudent();
+	s2.setName("Mahmoud");
+
+	s2.printStudent();
+
+
+
+}
